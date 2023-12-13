678c9f285a68417f698f473064f92920c82dd845
Tim <tusiime.andrews@gmail.com>
2023-12-13 13:33:33 +0000

    crikey

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -28,7 +28,8 @@ export default class extends Component {
 
     this.onChange(url)
 
-    this.isEasterEggBlack = true;
+     this.originalBackground = document.documentElement.style.background;
+    this.isOriginalBackground = true;
   }
 
 
@@ -53,47 +54,56 @@ export default class extends Component {
   
 
 
-onEasterEgg () {
-  // Toggle the color between white and black
-  const backgroundColor = this.isEasterEggBlack ? 'black' : 'white';
-  const fontColor = this.isEasterEggBlack ? 'white' : 'black';
-
-  // Apply the background color
-  document.documentElement.style.background = backgroundColor;
-  this.homeBottom.style.background = `linear-gradient(to bottom, transparent 0%, ${backgroundColor} 100%)`;
-  this.homeTop.style.background = `linear-gradient(to bottom, ${backgroundColor} 0%, transparent 100%)`;
-
-  // Apply the font color
-  document.documentElement.style.color = fontColor;
-  this.aboutGallery.style.color = fontColor;
-
-  // Update specific elements like "work" and "about"
-  const workElement = document.querySelector('.work'); // Replace '.work' with the actual class or ID
-  const aboutElement = document.querySelector('.home__link'); // Replace '.about' with the actual class or ID
-  if (workElement) workElement.style.color = fontColor;
-  if (aboutElement) aboutElement.style.color = fontColor;
-
-  // Update the navigation links
-  const navigationLinks = document.querySelectorAll('.navigation__link'); // Replace with the actual class or ID of your navigation links
-  navigationLinks.forEach(link => {
-    link.style.color = fontColor;
-  });
-
-  // Update the canvas background if it exists
-  if (this.canvas) {
-    const colorValue = this.isEasterEggBlack ? 0 : 255; // Black is 0, white is 255
-    this.canvas.background = {
-      r: colorValue,
-      g: colorValue,
-      b: colorValue
+  onEasterEgg() {
+    // Check if the current background is the original
+    if (this.isOriginalBackground) {
+      // Change the background to black
+      document.documentElement.style.background = 'black';
+  
+      // Update the background of other elements if necessary
+      this.homeBottom.style.background = 'linear-gradient(to bottom, transparent 0%, black 100%)';
+      this.homeTop.style.background = 'linear-gradient(to bottom, black 0%, transparent 100%)';
+      this.aboutGallery.style.color = 'white'; // Change text color for visibility
+  
+      // Update the canvas background color if it's part of your design
+      if (this.canvas) {
+        this.canvas.background = {
+          r: 0,
+          g: 0,
+          b: 0
+        };
+      }
+    } else {
+      // Revert to the original background color
+      document.documentElement.style.background = this.originalBackground;
+  
+      // Revert the background of other elements to their original state
+      this.homeBottom.style.background = ''; // Original style for homeBottom
+      this.homeTop.style.background = ''; // Original style for homeTop
+      this.aboutGallery.style.color = ''; // Original text color
+  
+      // Revert the canvas background color if it's part of your design
+      if (this.canvas) {
+        // Assuming you have a method or properties to get the original canvas background
+        this.canvas.background = this.getOriginalCanvasBackground();
+      }
+    }
+  
+    // Toggle the state
+    this.isOriginalBackground = !this.isOriginalBackground;
+  }
+  
+  // Example helper method to get the original canvas background
+  // You need to implement this based on how your canvas background is set
+  getOriginalCanvasBackground() {
+    // Return the original canvas background color
+    // This is just a placeholder, you need to implement this method
+    return {
+      r: 255, // Original red component
+      g: 255, // Original green component
+      b: 255  // Original blue component
     };
   }
-
-  // Toggle the state for the next click
-  this.isEasterEggBlack = !this.isEasterEggBlack;
-}
-
-
 
   /**
    * Listeners.
